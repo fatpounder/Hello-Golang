87a25c4175a5a6adfb493703da15c459a0228dce
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-04-28 19:38:39 -0500

    Create variables.go

---
+++ variables.go
@@ -0,0 +1,12 @@
+package main
+
+//Go is statically typed
+import "fmt"
+
+func main() {
+    a :=42
+    fmt.Println(a)
+
+    b, c, d, _, f := 0, 1, 2, 3, "happiness"
+    fmt.Println(b, c, d, f)
+}
