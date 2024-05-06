6ef27e3ce1c28ea3a330871dd65d51958d884851
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-05 20:49:33 -0500

    Update variables.go

--- variables.go
+++ variables.go
@@ -10,4 +10,5 @@ func variable() {
 	b, c, d, _, f := 0, 1, 2, 3, "happiness"
 	fmt.Println(b, c, d, f)
 	//all types have a zero value if nothing assigned to it yet
+	//boolean false, int 0, float 0.0, string "", others like pointers, slices, interfaces, etc. are nil
 }
