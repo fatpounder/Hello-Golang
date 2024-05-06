47d8ff137d5cca24ea541a79659876ec9ee15aeb
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-05 21:13:45 -0500

    Update printf.go

--- printf.go
+++ printf.go
@@ -10,10 +10,10 @@ func main() {
 	fmt.Printf("42 as hexadecimal, %x \n", adams)
 
 	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
-	fmt.Printf("%v \t %b \t %X",,,)
-	fmt.Printf("%v \t %b \t %X",,,)
-	fmt.Printf("%v \t %b \t %X",,,)
-	fmt.Printf("%v \t %b \t %X",,,)
-	fmt.Printf("%v \t %b \t %X",,,)
-	fmt.Printf("%v \t %b \t %X",,,)
+	fmt.Printf("%v \t %b \t %X", a, a, a)
+	fmt.Printf("%v \t %b \t %X", b, b, b)
+	fmt.Printf("%v \t %b \t %X", c, c, c)
+	fmt.Printf("%v \t %b \t %X", d, d, d)
+	fmt.Printf("%v \t %b \t %X", e, e, e)
+	fmt.Printf("%v \t %b \t %X", f, f, f)
 }
