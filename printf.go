000d34d575206039d516f21ee47294f0ad03c6fb
Marie Joiner <142051890+fatpounder@users.noreply.github.com>
2024-05-05 21:10:46 -0500

    Update printf.go

--- printf.go
+++ printf.go
@@ -8,4 +8,7 @@ func main() {
 	adams := 42
 	fmt.Printf("42 as binary, %b \n", adams)
 	fmt.Printf("42 as hexadecimal, %x \n", adams)
+
+	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+	fmt.Printf("%v \t %b \t %X",,,)
 }
